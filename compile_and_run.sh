eeb1e97757257b1fba13e90b54b4f8c029394717
Thiago Rodrigues <90068572+trsaints@users.noreply.github.com>
2024-09-14 17:08:40 +0000

    feat: add a shellscript to compile and run the algorithms on the go

---
+++ compile_and_run.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+if [ -z "$1" ]; then
+  echo "Usage: $0 <filepath_without_extension>"
+  exit 1
+fi
+
+filepath=$1
+
+gcc -o "${filepath}.o" "${filepath}.c"
+
+if [ $? -ne 0 ]; then
+  echo "Compilation failed."
+  exit 1
+fi
+
+./"${filepath}.o" < inputs.txt
