9ddf140f8e30a58eed8cc2b162041064e8596405
Thiago Rodrigues <90068572+trsaints@users.noreply.github.com>
2024-09-15 13:26:20 +0000

    feat: solve challenge 1075

---
+++ beecrowd/beginner/challenge1075.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <string.h>
+ 
+int main() {
+    int N;
+
+    char line_buffer[32];
+    char result_buffer[262144];
+
+    if (fgets(line_buffer, sizeof(line_buffer), stdin))
+        sscanf(line_buffer, "%d", &N);
+
+    for (int i = 1; i < 10000; i++) {
+        if ((i % N) != 2) continue;
+
+        char tmp_buffer[32];
+
+        sprintf(tmp_buffer, "%d", i);
+        strcat(result_buffer, tmp_buffer);
+        strcat(result_buffer, "\n");
+    }
+
+    printf("%s", result_buffer);
+    
+    return 0;
+}
