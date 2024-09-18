92a9be6bc41f6765d3698529f91dad76033e8719
Thiago Rodrigues <90068572+trsaints@users.noreply.github.com>
2024-09-18 02:30:10 +0000

    feat: solve challenge 1113

---
+++ beecrowd/beginner/challenge1113.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+    static char read_buffer[512];
+    char *read_position = read_buffer,
+         *read_end = read_buffer + sizeof(read_buffer);
+
+    static char result_buffer[4096];
+    char *result_position = result_buffer,
+         *result_end = result_buffer + sizeof(result_buffer);
+
+    int start, end;
+    char message[14];
+
+    while (result_position < result_end) {
+        if (!fgets(read_buffer, sizeof(read_buffer), stdin)) 
+        return 1;
+
+        sscanf(read_buffer, "%d %d", &start, &end);
+
+        if (start == end) break;
+    
+        if (start < end) 
+            strcpy(message, "Crescente\n");
+        else
+            strcpy(message, "Decrescente\n");
+
+        result_position += snprintf(result_position,
+                                    result_end - result_position,
+                                    "%s",
+                                    message);
+    }
+
+    fputs(result_buffer, stdout);
+
+    return 0;
+}
