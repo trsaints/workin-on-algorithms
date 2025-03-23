3296addb6e0165e036c710c3de4587089d62517c
trsaints <thiago.dev2k3@gmail.com>
2025-03-22 22:45:03 -0300

    fix: adjust memory allocation and add strict checks before performing any modification in the stack

--- dsa/stack_with_pointers.c
+++ dsa/stack_with_pointers.c
@@ -4,13 +4,16 @@
 
 struct StackP
 {
+    unsigned *base;
     unsigned *top;
+    unsigned *end;
     size_t length;
 };
 
-unsigned stackp_push(struct StackP *stackp, int x);
-unsigned stackp_pop(struct StackP *stackp);
+int stackp_push(struct StackP *stackp, int x);
+int stackp_pop(struct StackP *stackp);
 struct StackP *stackp_init(size_t length);
+void stackp_free(struct StackP *stackp);
 
 int main(void)
 {
@@ -31,6 +34,8 @@ int main(void)
 
     printf("popped %d from stack.\n", popped);
     printf("stack->top: %d\n", *(my_stack->top - 1));
+
+    stackp_free(my_stack);
 }
 
 struct StackP *stackp_init(size_t length)
@@ -40,26 +45,40 @@ struct StackP *stackp_init(size_t length)
     if (!new_stack)
         return NULL;
 
-    new_stack->top = (unsigned *)calloc(1, length * sizeof(unsigned));
+    new_stack->base = (unsigned *)calloc(1, length * sizeof(unsigned));
+    new_stack->top = new_stack->base;
+    new_stack->end = new_stack->base + length;
     new_stack->length = length;
 
     return new_stack;
 }
 
-unsigned stackp_push(struct StackP *stackp, int x)
+int stackp_push(struct StackP *stackp, int x)
 {
+    if (stackp->top >= stackp->end)
+        return -1;
+
     *(stackp->top) = x;
     stackp->top++;
 
     return x;
 }
 
-unsigned stackp_pop(struct StackP *stackp)
+int stackp_pop(struct StackP *stackp)
 {
+    if (stackp->top <= stackp->base)
+        return -1;
+
     int popped = *(stackp->top - 1);
 
-    *(stackp->top) = 0;
     stackp->top--;
+    *(stackp->top) = 0;
 
     return popped;
+}
+
+void stackp_free(struct StackP *stackp)
+{
+    free(stackp->base);
+    free(stackp);
 }
